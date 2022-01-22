2c2b719050a86146acaf2a08e3a4a7c002bb7175
ManasBommakanti <manas.bommakanti@gmail.com>
2022-01-22 13:19:19 -0500

    Started new project lol

--- DogBreeds.py
+++ DogBreeds.py
@@ -28,11 +28,30 @@ class Conv:
         output = np.zeros((h - 2, w - 2, self.num_filters))
 
         for im_region, i, j in self.iterate_regions(input):
-            #print(str(im_region) + " " + str(i) + " " + str(j))
-            #print(im_region)
-            #print(self.filters)
-            #print(mult)
             output[i, j] = np.dot(im_region, self.filters).sum()
+
+        return output
+
+
+class Pool:
+    # A Max Pooling layer using a pool size of 2.
+
+    def iterate_regions(self, image):
+        h, w, _ = image.shape
+        new_h = h // 2
+        new_w = w // 2
+
+        for i in range(new_h):
+            for j in range(new_w):
+                im_region = image[(i * 2):(i * 2 + 2), (j * 2):(j * 2 + 2)]
+                yield im_region, i, j
+
+    def forward(self, input):
+        h, w, num_filters = input.shape
+        output = np.zeros((h // 2, w // 2, num_filters))
+
+        for im_region, i, j in self.iterate_regions(input):
+            output[i, j] = np.amax(im_region, axis=(0, 1))
 
         return output
 
@@ -42,5 +61,9 @@ img = img.resize((28, 28))
 imgGray = img.convert('L')
 imgGrayArray = array(img)
 
-conv = Conv(8)
-print(conv.forward(imgGrayArray).shape)
+conv = Conv(10)
+convOutput = conv.forward(imgGrayArray)  # 26, 26, 10
+pool = Pool()
+poolOutput = pool.forward(convOutput)  # 13, 13, 10
+
+print(poolOutput.shape)
