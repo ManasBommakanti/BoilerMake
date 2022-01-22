fc66a091908b24ebcc637ca5ff17bf670822f2cf
ManasBommakanti <manas.bommakanti@gmail.com>
2022-01-22 13:23:33 -0500

    Started new project lol

---
+++ OpenCSV.py
@@ -0,0 +1,21 @@
+import csv
+import requests
+
+all_data = {'USA': 840, 'China': 156, 'United Kingdom': 826, 'India': 356, 'Russia': 643}
+year = list(range(1950, 2021, 10))
+
+# Define the remote URL
+country = input()
+
+for x in year:
+    url = "https://www.populationpyramid.net/api/pp/" + str(all_data[country]) + "/" + str(x) + "/?csv=true"
+    # Send HTTP GET request via requests
+    data = requests.get(url)
+    # Convert to iterator by splitting on \n chars
+    lines = data.text.splitlines()
+    # Parse as CSV object
+    reader = csv.reader(lines)
+
+    # View Result
+    for row in reader:
+        print(row)
