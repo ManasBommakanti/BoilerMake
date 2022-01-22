c7fbd0beec809d6263fd1787b64e1b2182029b2c
ManasBommakanti <manas.bommakanti@gmail.com>
2022-01-21 23:07:36 -0500

    Grayscale and convolution class

--- DogBreeds.py
+++ DogBreeds.py
@@ -1,4 +1,34 @@
 import http.client
+import numpy as np
+from PIL import Image
+
+class Conv:
+    # Convolution layer
+
+    def __init__(self, num_filters):
+        self.num_filters = num_filters
+
+        # filters is a 3d array with dimensions (num_filters, 3, 3)
+        # We divide by 9 to reduce the variance of our initial values
+        self.filters = np.random.randn(num_filters, 3, 3) / 9
+
+    def iterate_regions(self, image):
+        h, w = image.shape
+
+        for i in range(h - 2):
+            for j in range(w - 2):
+                im_region = image[i:(i + 3), j:(j + 3)]
+                yield im_region, i, j
+
+    def forward(self, input):
+        h, w = input.shape
+        output = np.zeros((h - 2, w - 2, self.num_filters))
+
+        for im_region, i, j in self.iterate_regions(input):
+            output[i, j] = np.sum(im_region * self.filters, axis=(1, 2))
+
+        return output
+
 
 conn = http.client.HTTPSConnection("api.thedogapi.com")
 
@@ -8,5 +38,8 @@ conn.request("GET", "/v1/breeds?attach_breed=0", headers=headers)
 
 res = conn.getresponse()
 data = res.read()
-
 print(data.decode("utf-8"))
+
+img = Image.open('apple_test.jpeg')
+imgGray = img.convert('L')
+imgGray.save('test_gray.jpg')
